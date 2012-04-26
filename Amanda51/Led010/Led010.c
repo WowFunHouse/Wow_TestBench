1a2fb7b0e3ea6f756477cd9cf215e333d855a339
Amanda <gz_lyra@163.com>
2012-04-26 15:03:51 +0800

    add grtting faster

--- Amanda51/Led010/Led010.c
+++ Amanda51/Led010/Led010.c
@@ -14,7 +14,6 @@ void delay01(void)
 	unsigned int	a;
 
 	for(a=0; a<LOOPSA; a++);
-
 }
 
 void delay02(void)
@@ -22,7 +21,13 @@ void delay02(void)
 	unsigned int	a;
 
 	for(a=0; a<LOOPSB; a++);
+}
 
+void delay(unsigned int t)
+{
+	unsigned int	a;
+
+	for(a=0; a<t; a++);
 }
 
 void main(void)
@@ -33,18 +38,20 @@ void main(void)
 	for(;;)
 	{
 		P0=led(a);
-		delay01();
+		delay(LOOPSA);
 
 		P0=led(0);
-		delay02();
+		delay(30000);
 		P0=led(3);
-		delay02();
+		delay(15000);
 		P0=led(12);
-		delay02();
+		delay(7500);
 		P0=led(48);
-		delay02();
+		delay(3500);
 		P0=led(192);
-		delay02();
+		delay(1750);
+		P0=led(0);
+		delay(800);
 
 		a++;
 	}
