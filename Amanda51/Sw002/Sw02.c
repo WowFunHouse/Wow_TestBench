5fca4a92b5fd228636a338a277dde02e692f0910
Amanda <gz_lyra@163.com>
2012-05-08 15:03:52 +0800

    The first commit switch, have function of led and delay loops and switch A to D

---
+++ Amanda51/Sw002/Sw02.c
@@ -0,0 +1,93 @@
+#include <STC89.H>
+
+char led(char x)
+{
+	return	~x;
+}
+
+void delay(int t)
+{
+	int		x;
+
+	for(x=1; x<t; x++);
+}
+
+void swicha(void)
+{
+	int 	a;
+	int		n;
+
+	a=0x01;
+
+	//for(;;){
+		for(n=0; n<=8; n++)
+		{
+			P0=led(a<<n);
+			delay(20000);
+		}
+	//}
+}
+
+void swichb(void)
+{
+	int 	a;
+	int		n;
+
+	a=0x80;
+
+	// for(;;){
+		for(n=0; n<=8; n++)
+		{
+			P0=led(a>>n);
+			delay(20000);
+		}
+	//}
+}
+
+void swichc(void)
+{
+	// for(;;){
+		P0=led(0x81);
+		delay(20000);
+		P0=led(0x42);
+		delay(20000);
+		P0=led(0x24);
+		delay(20000);
+		P0=led(0x18);
+		delay(20000);
+	//}
+}
+
+void swichd(void)
+{
+	// for(;;){
+		P0=led(0x18);
+		delay(20000);
+		P0=led(0x24);
+		delay(20000);
+		P0=led(0x42);
+		delay(20000);
+		P0=led(0x81);
+		delay(20000);
+	//}
+}
+
+void main(void)
+{
+	int		x;
+
+	P2=0xFF;
+
+	for(;;)
+	{
+		x=P2;
+		x=x&0x0F;
+
+		if		(x==0x0E)		swicha();
+		else if	(x==0x0D)		swichb();
+		else if	(x==0x0B)		swichc();
+		else if	(x==0x07)		swichd();
+
+		else					P0=led(0x00);
+	}
+}
