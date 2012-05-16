43c0fc6f21e6d3065722036d2a02f225d2ba624a
Mike <SpNovaDev@gmail.com>
2012-05-16 13:06:20 +0800

    Add Funbox main()

---
+++ FunBox51A/Test/FunBox/funbox_main001.c
@@ -0,0 +1,19 @@
+#include <STC89.H>
+#include "../../include/library.h"
+#include "../../include/funbox51a.h"
+
+void main(void)
+{
+	unsigned char sw;
+
+	for (;;)
+	{
+		sw=getsw();
+		if (sw !=0)
+		{
+			ledptn(sw);
+			beepsnd(1000,50);
+			delay(10000);
+		}
+	}
+}
