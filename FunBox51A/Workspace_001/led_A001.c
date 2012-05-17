e128fea99f09af5e12355dabf619523db41bfdd0
Mike <SpNovaDev@gmail.com>
2012-05-17 18:00:05 +0800

    Setup the templates

--- FunBox51A/Workspace_001/led_A001.c
+++ FunBox51A/Workspace_001/led_A001.c
@@ -1,6 +1,6 @@
 #include <STC89.H>
-
-#include "../../include/funbox51a.h"
+#include "../include/library.h"
+#include "../include/funbox51a.h"
 
 void ledptn(unsigned char pattern)
 {
--- FunBox51A/include/library.h
+++ FunBox51A/include/library.h
@@ -1,1 +1,4 @@
+void delayms(unsigned int delaytime);	  	// Delay time in ms
+void delaysec(unsigned int delaytime); 		// Delay time in seconds
+
 void delay(unsigned int delaytime);
