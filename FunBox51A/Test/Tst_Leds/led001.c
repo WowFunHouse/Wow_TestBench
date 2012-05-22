6ed555ae167e29c1227d99a2e348f7792d1f3127
Joey <joeycao_wow@163.com>
2012-05-22 13:37:39 +0800

    update joey files

--- FunBox51A/Test/Tst_Leds/led001.c
+++ FunBox51A/Test/Tst_Leds/led001.c
@@ -7,25 +7,49 @@ void ledptn(unsigned char pattern)
  unsigned int t;
 
  if (pattern == 1)
- {
-  	P0 = 0x80;
-	for (t=0; t<10000; t++);
-	P0 = 0x40;
- 	for (t=0; t<10000; t++);
-	P0 = 0x20;
-	for (t=0; t<10000; t++);
-	P0 = 0x10;
-  	for (t=0; t<10000; t++);
- }
+ 	{
+  		P0 = 0x01;
+		for (t=0; t<10000; t++);
+		P0 = 0x02;
+ 		for (t=0; t<10000; t++);
+		P0 = 0x04;
+		for (t=0; t<10000; t++);
+		P0 = 0x80;
+  		for (t=0; t<10000; t++);
+ 	}
  else if (pattern == 2)
- {
- }
+ 	{
+ 		P0=0x80;
+		for (t=0; t<10000; t++);
+		P0=0x20;
+		for (t=0; t<10000; t++);
+		P0=0x40;
+		for (t=0; t<10000; t++);
+		P0=0x01;
+		for (t=0; t<10000; t++);	
+ 	}
  else if (pattern == 3)
- {
- }
+ 	{
+		P0=0x01;
+		for (t=0; t<10000; t++);
+		P0=0x42;
+		for (t=0; t<10000; t++);
+   		P0=0x24;
+		for (t=0; t<10000; t++);
+		P0=0x18;
+		for (t=0; t<10000; t++);
+ 	}
  else if (pattern == 4)
- {
- }
+ 	{
+		P0=0x18;
+		for (t=0; t<10000; t++);
+		P0=0x24;
+		for (t=0; t<10000; t++);
+		P0=0x42;
+		for (t=0; t<10000; t++);
+		P0=0x81;
+		for (t=0; t<10000; t++);
+ 	}
 
 }
 
