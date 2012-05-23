765f4563ca447ff03abd81af826a6dcc596b3623
Amanda <gz_lyra@163.com>
2012-05-23 19:41:17 +0800

    The first edition of switch program

--- Amanda51/Sw003/Sw03.c
+++ Amanda51/Sw003/Sw03.c
@@ -3,20 +3,50 @@
 unsigned char led(unsigned char n)
 {
 	return	~n;
-}
+} /* led() */
 
-unsigned char main(void)
+unsigned char getsw(void)
 {
-	unsigned char	b, x;
+	unsigned char	x, k;
 
+	P2 = 0xFF;				// Pull Up the Output Pins to check Input
+
+	x = P2;					// Check switch status
+	x = x&0x0F;				// Filter out the junks
+	
+	switch (x) {
+		 case 0x0E:		k=1;   		// Switch #1 is ON
+		 				break;
+
+		 case 0x0D:		k=2;	 	// Switch #2 is ON
+		 				break;
+
+		 case 0x0B:		k=3;	 	// Switch #3 is ON
+		 				break;
+
+		 case 0x07:		k=4; 		// Switch #4 is ON
+		 				break;
+
+		 default:		k=0;  		// No switch is pressed
+		 				break;
+	}
+	return k;  						// Return the Switch status
+} /* getsw() */
+
+void main(void)
+{
+	unsigned char	a, b, l;
+
+	l = 0;
 	b = 0x00;
 
-	P2 = 0xFF;
-	x = P2;
-	x = x&0x0F;
-	
-	if(x == 0x0E)	return ~b;
-	else			return b;
+	for(;;)
+	{
+		a = getsw();
 
-	P0 = led(b);
+		if((a != 0) && (a != l))	b=~b;
+
+		l = a;
+		P0 = led(b);
+	}
 }
