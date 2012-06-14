c7b600f9e64af496f6458fef4ec6b73126e2c449
Cathy Du <catdx7@gmail.com>
2012-06-14 20:22:36 +0800

    add comments

--- Cathy51/Exam_A01/led001.c
+++ Cathy51/Exam_A01/led001.c
@@ -2,14 +2,14 @@
 
 void main(void)
 {	
-	unsigned int t;
+//	unsigned int t;
 
-	P0 = 0xff;
+//	P0 = 0xff;	   CPU begins execution from location 0000H	P0 = 11111111
 	
-	for(;;)
-	{
-	 	P0 = 0xb7;
-		for (t=0; t<10000; t++);	
-	}
-
+//	for(;;)
+//	{
+	P0 = 0xb7;
+//		for (t=0; t<10000; t++);	 in the for loop P0 ON
+//	}	
+	for(;;);
 }
