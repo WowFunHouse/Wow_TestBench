f3a95d7d99509895bad380a79122218bc91a06de
Mike <SpNovaDev@gmail.com>
2012-06-19 20:24:51 +0800

    Template for general C source with RZ-51 board

---
+++ Common51/tmpl_rz51_01.c
@@ -0,0 +1,26 @@
+/**************************************************************
+ Title:			
+ Version:		1.0
+
+ Description:	
+ Created on:	2012-06-19
+ Created by:	Michael
+
+ Hardware:		RichMCU RZ-51
+					   
+ Connections:	P0.0 - P0.7 -> 7-Segment LED segemnt control (0:On 1:Off)
+				P2.0 - P2.1 -> Digit selection of 7-Segment LED
+ 			 	P1.4 -> Switch K5 (O:On 1:Off)	
+				
+ **************************************************************/
+#include <STC89.H>
+#include "../../Common51/lib51.h"
+
+void main(void)
+{
+	for(;;)
+	{
+
+	}
+}
+
