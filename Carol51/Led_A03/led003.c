5aac8f13c18702cb46af7ed64ac154df4f137aa5
Carol <cardev88@gmail.com>
2012-07-09 23:22:27 +0800

    3rd LED Test

---
+++ Carol51/Led_A03/led003.c
@@ -0,0 +1,30 @@
+#include <STC89.H>
+
+void delay(void)
+{
+     unsigned int t;
+
+     for(t=0; t<30000; t++);
+}
+void main(void)
+{
+	 unsigned char n;
+
+     for(n=0; n<14; n++)
+     {
+	       P0=~0x48;
+		   delay();
+	
+		   P0=~0x00;
+		   delay();
+	   
+		   if((n==2)||(n==4)||(n==8)||(n==13))
+		   {
+			   delay();
+			   delay();
+			   delay();
+			   delay();
+			   delay();
+	       }
+	   }
+}
