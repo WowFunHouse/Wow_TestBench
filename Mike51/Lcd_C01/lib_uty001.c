71ea0dddc1769f5b2513921d1fa899743cddc4bf
Cathy Du <catdx7@gmail.com>
2012-07-09 18:58:05 +0800

    Unfinished partitioning of LCD driver

---
+++ Mike51/Lcd_C01/lib_uty001.c
@@ -0,0 +1,17 @@
+/*************************************
+ Library of supporting utilities
+
+ Version:		0.01
+ Description:	Supporting functions
+
+ Created on:	2012-07-09
+ Created by:	Michael
+/*************************************/
+
+void delay(unsigned int delaycount)
+{
+	unsigned int t;
+
+	for (t=0; t<delaycount; t++);
+
+} /* delay */
