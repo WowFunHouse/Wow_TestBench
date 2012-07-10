1c52d527bce03e3c9577a566f2fcc6315b854868
Cathy Du <catdx7@gmail.com>
2012-07-10 16:55:11 +0800

    Repartition all files

---
+++ Cathy51/Lcd_C01/lcd_lib001.h
@@ -0,0 +1,20 @@
+ /******************************************************************
+Library of supporting utilities
+
+ Version:      		0.01
+ Description:		Supporting function
+
+ Created on:		2012-07-09
+ Created by:		Cathy
+ ******************************************************************/
+
+unsigned char lcdCheckBusy(void);
+void lcdWaitUntilReady(void);
+
+void lcdWriteCmd(unsigned char cmd);
+void lcdWriteData(unsigned char dData);
+void lcdWriteString(char *str);
+ 
+void lcdSelectRow(unsigned char row);  	// row: 0, 1
+
+void lcdClearScreen(void);
