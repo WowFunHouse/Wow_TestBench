44812794a84ec583c529a1b756c90f05b335b78d
Cathy Du <catdx7@gmail.com>
2012-07-12 23:20:51 +0800

    LCD animated character test

--- Cathy51/Lcd_C01/lcd_lib001.c
+++ Cathy51/Lcd_C01/lcd_lib001.c
@@ -1,14 +1,16 @@
 /****************************************************************************
  LCD Driver Library
 
- Version:		0.01
+ Version:		1.0
  Description:	LCD 1602 Driver
 
  Created on:		2012-07-09
  Created by:		Cathy
 
- Hardware:			RichMCU RZ-51
- Connections:		P20 -> RS
+ Hardware:			RichMCU RZ-51V2.0
+
+ Connections:		LCD pins as below-
+ 					P20 -> RS
  					P21 -> RW
 					P22 -> EN
 					P0  -> DATAPORT
@@ -22,8 +24,7 @@
 					JPSMG 数码管控制跳线拔掉使数码管停止工作。
 					JPBG 液晶背光跳线插上
 					插上1602LCD液晶
-
-****************************************************************************/
+ ****************************************************************************/
 #include <STC89.H>
 #include "lib_uty001.h"
 #include "hw_rz51v2.h"
@@ -113,7 +114,8 @@ void lcdSelectRow(unsigned char row)  	// row: 0, 1
 	else					   			// row != 0
 	{
 		lcdWriteCmd(0x80 | 0x40);		//	0xC0
-	}	
+	}
+		
 } /* lcdSelectRow */
 
 void lcdInit(void)
@@ -131,4 +133,66 @@ void lcdSetInputMode(unsigned char input_mode ,unsigned char input_shift)
 {
 	lcdWriteCmd(0x04 | input_mode |input_shift);
 }
+
+
+/***************************************************************************
+lcdMakeRawFont() - Use thisto create a new font
+Input:
+	c: chraacter code (0-7)
+	row0 - row7: dotmatrix colums from
+****************************************************************************/
+void lcdMakeRawFont(unsigned char c, unsigned char row0, 
+									 unsigned char row1,
+									 unsigned char row2,
+									 unsigned char row3,
+									 unsigned char row4,
+									 unsigned char row5,
+									 unsigned char row6,
+									 unsigned char row7)
+{
+	unsigned char cgAddr = 8*c;
+
+	lcdWriteCmd(0x40| cgAddr);
+	lcdWriteData(row0);
+
+	lcdWriteCmd(0x40| cgAddr+1);
+	lcdWriteData(row1);
+
+	lcdWriteCmd(0x40| cgAddr+2);
+	lcdWriteData(row2);
+
+	lcdWriteCmd(0x40| cgAddr+3);
+	lcdWriteData(row3);
+
+	lcdWriteCmd(0x40| cgAddr+4);
+	lcdWriteData(row4);
+
+	lcdWriteCmd(0x40| cgAddr+5);
+	lcdWriteData(row5);
+
+	lcdWriteCmd(0x40| cgAddr+6);
+	lcdWriteData(row6);
+
+	lcdWriteCmd(0x40| cgAddr+7);
+	lcdWriteData(row7);
+}
+
+/***************************************************************************
+lcdMakeRawFont() - Use thisto create a new font
+Input:
+	c: chraacter code (0-7)
+	row0 - row7: dotmatrix colums from top to bottom
+				 valid bits b0 - b4 from right to left
+Output: N/A
+****************************************************************************/
+
+//void lcdMakeFont(unsigned char *row)
+//{
+//	for (n=0, n<8, n++)
+//	{
+//		lcdWritecmd(0x40|*row)
+//
+//	}
+//}
+
 
--- Cathy51/Lcd_C01/lcd_lib001.h
+++ Cathy51/Lcd_C01/lcd_lib001.h
@@ -37,16 +37,33 @@ Library of supporting utilities
 void lcdWriteData(unsigned char dData);
 void lcdWriteString(char *str);
  
-void lcdSelectRow(unsigned char row);  	// row: 0, 1
+void lcdSelectRow(unsigned char row);  		// row: 0, 1
 void lcdClearRow(unsigned char row);
 void lcdClearCurrentRow(void);
 void lcdClearRestOfRow(void);
-
+																    
 void lcdSetDisplayMode(unsigned char display_mode); 
 
 void lcdSetInputMode(unsigned char input_mode ,unsigned char input_shift);
+
+/***************************************************************************
+lcdMakeRawFont() - Use thisto create a new font
+Input:
+	c: chraacter code (0-7)
+	row0 - row7: dotmatrix colums from top to bottom
+				 valid bits b0 - b4 from right to left
+Output: N/A
+ ****************************************************************************/
+void lcdMakeRawFont(unsigned char c, unsigned char row0, 
+									 unsigned char row1,
+									 unsigned char row2,
+									 unsigned char row3,
+									 unsigned char row4,
+									 unsigned char row5,
+									 unsigned char row6,
+									 unsigned char row7);
 						
 void lcdInit(void);		   //2lines, Font:5x7
 
-
 void lcdClearScreen(void);
+
