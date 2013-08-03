5faf1388876e93dfbc00145a09763e603829b2b7
Cathy Du <catdx7@gmail.com>
2013-08-04 00:04:55 +0800

    Interrupt Testing

---
+++ Cathy51/INTR_A01/intr001.c
@@ -0,0 +1,56 @@
+ /****************************************************************************
+ Title:			Interrupt testing
+ 
+ File:			 intr002.c
+
+ Version:		0.01
+ Description:	Toggle LEDs on Port-1
+
+ Created on:	2013-08-2
+ Created by:	
+
+ Board:			RichMCU RZ-51V2.0
+
+ Connections:	Default
+
+ Jumpers:		All on		
+ **************************************************************/
+#include <STC89.H>
+
+void delay(void)
+{
+	int d;
+	for (d=0; d<10000; d++);
+}/* delay */
+
+void main(void)
+{	 
+	char i = 0;
+
+	P1=0xff;
+	EX0=1;
+	EA=1;
+
+	for(;;)
+	{
+	  	P0=i++;
+		delay();
+	}
+} /* main */
+//void isr(void) interrupt 1 using 2
+//{
+//	P1=~P1; 
+//
+//} /* isr */ 
+
+void isr(void) interrupt 1 using 2
+{
+	EA=0;
+	delay();
+	delay();
+	delay();
+	P1=~P1;
+	EA=1; 
+
+} /* isr */
+
