1b11967e20adf9c1d7604c4482180a8e0086d7d6
Cathy Du <catdx7@gmail.com>
2013-08-04 20:57:06 +0800

    resolve keybounc

--- Cathy51/INTR_A01/intr_b010_001.c
+++ Cathy51/INTR_A01/intr_b010_001.c
@@ -1,7 +1,7 @@
  /****************************************************************************
  Title:			Interrupt testing
  
- File:			 intr002.c
+ File:			intr002.c
 
  Version:		0.01
  Description:	Toggle LEDs on Port-1
@@ -17,39 +17,57 @@
  **************************************************************/
 #include <STC89.H>
 
+#define	KEYBOUNCE_MASK_US	6000
+
 void delay(void)
 {
-	int d;
+	unsigned int d;
+
 	for (d=0; d<10000; d++);
+
 }/* delay */
 
 void main(void)
-{	 
-	char i = 0;
+{
+	unsigned char i=0;
 
-	P1=0xff;
-	EX0=1;
-	EA=1;
+	P1 = 0xff;					// Turn off LEDs
+
+	IT0 = 1;						// Int0 Trigger: Edge
+	EX0 = 1;						// Int0: On
+
+	ET0 = 0;					// Timer0 Interrupt: Off
+	TR0 = 0;					// Timer0: Off
+
+	EA  = 1;					// Enable ALL Interrupts
 
 	for(;;)
 	{
-	  	P0=i++;
+	  	P0=~(i++);
 		delay();
 	}
 } /* main */
-//void isr(void) interrupt 1 using 2
-//{
-//	P1=~P1; 
-//
-//} /* isr */ 
 
-void isr(void) interrupt 1 using 2
+void isrKeyboard(void) interrupt 0			// Keyboard Interrupt Service Routine
 {
-	EA=0;
-	delay();
-	delay();
-	delay();
-	P1=~P1;
-	EA=1; 
+	EX0 = 0;								// Int0: Off
 
-} /* isr */
+	P1 = ~P1;
+
+	TMOD = T0_M1;							// Timer0: Mode1
+
+	TH0  = (65536 - KEYBOUNCE_MASK_US) >>8; // Timer0 delay time
+	TL0  = (unsigned char)((65536 - KEYBOUNCE_MASK_US) & 0x00ff);
+
+	ET0  = 1;								// Timer0 Interrupt: On
+	TR0	 = 1;								// Timer0: Start now...
+	
+} /* isrKeyboard */
+
+void isrTimer0(void) interrupt 1			// Timer0 Interrupt Service Routine 
+{
+	ET0 = 0;								// Timer0 Interrupt: Off
+
+	TR0 = 0;								// Timer0: Off
+	EX0 = 1;								// Int0: On
+} /* isrTimer0 */
